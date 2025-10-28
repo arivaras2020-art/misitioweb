08a10d0134987bb7c41725263f55a1f51f03a8f9
ariadna <arivaras2020@gmail.com>
2025-10-28 16:19:43 -0300

    trabajamos en el inicio

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,11 @@
-// --- MENÚ HAMBURGUESA ---
-const menu = document.getElementById('mobile-menu');
-const overlay = document.getElementById('overlay');
+const menuBtn = document.getElementById("menu-btn");
+const closeMenu = document.getElementById("close-menu");
+const sideMenu = document.getElementById("side-menu");
 
-menu.addEventListener('click', () => {
-  overlay.style.height = overlay.style.height === '100%' ? '0' : '100%';
-  menu.classList.toggle('open');
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.add("active");
 });
 
-// --- SCROLL REVEAL EFECTO ---
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-  const reveals = document.querySelectorAll('.reveal');
-  for (let i = 0; i < reveals.length; i++) {
-    const windowHeight = window.innerHeight;
-    const elementTop = reveals[i].getBoundingClientRect().top;
-    const revealPoint = 100;
-
-    if (elementTop < windowHeight - revealPoint) {
-      reveals[i].classList.add('active');
-    } else {
-      reveals[i].classList.remove('active');
-    }
-  }
-}
+closeMenu.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+});
